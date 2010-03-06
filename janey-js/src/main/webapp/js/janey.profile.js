e1b8bdcaa3947ecec927e92d9b8f28e7318a14f1
tikiwade <wade@tikiwade.com>
2010-03-05 23:02:59 -0600

    added the javascript project

---
+++ janey-js/src/main/webapp/js/janey.profile.js
@@ -0,0 +1,23 @@
+dependencies = {
+		layers:[{name:"janeydojo.js",
+			dependencies:[
+			              "dijit.Dialog",
+			              "dijit.Editor",
+			              "dijit.ProgressBar",
+			              "dijit.TitlePane",
+			              "dijit.Tooltip",
+			              "dijit.form.Button",
+			              "dijit.form.FilteringSelect",
+			              "dijit.form.NumberTextBox",
+			              "dijit.form.TextBox",
+			              "dijit.layout.ContentPane",
+			              "dijit.layout.TabContainer",
+			              "dojox.widget.Toaster"
+			              
+			             ]
+			   }],
+		prefixes:[
+		          ["dijit", "../dijit"],
+		          ["janey", "../../janey"]
+		         ]
+};
